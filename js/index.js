d1cdb4e61ec5e132ebae245b4f7478b9835a7f5e
tryyang2001 <e0550472@u.nus.edu>
2022-08-02 11:11:00 +0800

    created neetcode 150 writeups pages

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const allTopicLinks = document.querySelectorAll(".topic-link");
+
+allTopicLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    let fileName =
+      "./topics/" + link.className.replace("topic-link ", "") + ".html";
+    location.href = fileName;
+  });
+});
+
+const allLinks = document.getElementsByTagName("a");
+for (var i = 0; i < allLinks.length; i++) {
+  allLinks[i].target = "_blank";
+}
+
+const homeBtn = document.querySelector(".home-btn");
+if (homeBtn !== null) {
+  homeBtn.children[0].target = "";
+}
